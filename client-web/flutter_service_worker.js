b34bd26d829047625c8893c62f6edaec208d06b0
Mikael Wills <mikael.wills@ideaworks.co.uk>
2025-12-13 23:16:29 +0000

    Web client navigation consistencies

--- client-web/flutter_service_worker.js
+++ client-web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5563ffbfedead030786e49ca66402dc1",
+const RESOURCES = {"flutter_bootstrap.js": "0520333eb84f49821c5a823ad5e48289",
 "version.json": "06e0dc074ffd201f1318551ce3b2b770",
 "index.html": "25de61404b4502ac41d2b77cbef91f20",
 "/": "25de61404b4502ac41d2b77cbef91f20",
-"main.dart.js": "95b49b9c992556b07474088f9a0f122b",
+"main.dart.js": "547e191e9dbac8a3fdc4117c58c3cbe3",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "4b904ddf5688ea85941b803fa715f901",
 "icons/Icon-192.png": "28f579778a70279d127188836cf6cce0",
