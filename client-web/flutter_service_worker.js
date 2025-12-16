ec598f91eb72f58d08a6fbc4a652d8221e48784d
Mikael Wills <mikael.wills@ideaworks.co.uk>
2025-12-16 11:12:47 +0000

    preview/raw toggle

--- client-web/flutter_service_worker.js
+++ client-web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "59b70679ff8748809aab4bb6f53e2404",
+const RESOURCES = {"flutter_bootstrap.js": "d12e9dbf90533183bf7725a841ec6b23",
 "version.json": "06e0dc074ffd201f1318551ce3b2b770",
 "index.html": "25de61404b4502ac41d2b77cbef91f20",
 "/": "25de61404b4502ac41d2b77cbef91f20",
-"main.dart.js": "3411d3c063db9c058ac602430411162d",
+"main.dart.js": "969c3a3fd6e2caf2c671562b0c0dbb87",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "4b904ddf5688ea85941b803fa715f901",
 "icons/Icon-192.png": "28f579778a70279d127188836cf6cce0",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "59b70679ff8748809aab4bb6f53e2404",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "79953bdfdb14330e493eddc53c6684b9",
+"assets/fonts/MaterialIcons-Regular.otf": "bbcb5d886460c8bf732a71c0853ef208",
 "canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
 "canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da4",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
