548f8ce524d1a20e342ace525864458d81dee577
Mikael Wills <mikael.wills@ideaworks.co.uk>
2026-07-25 15:57:17 +0100

    client-web: rebuild bundle with verbatim path sanitizer

--- client-web/flutter_bootstrap.js
+++ client-web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2098679888" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1626877138" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
